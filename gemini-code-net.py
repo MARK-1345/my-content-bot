57d0958f9f645d7f2294a3562e0bc8aa21240301
MARK-1345 <polode90@gmail.com>
2026-05-07 21:50:23 +0700

    Update gemini-code-net.py

--- gemini-code-net.py
+++ gemini-code-net.py
@@ -39,7 +39,7 @@ if not nickname:
 try:
     api_key = st.secrets["GOOGLE_API_KEY"]  # ใช้ตอน deploy บน Streamlit Cloud
 except:
-    api_key = "AIzaSyCBia1yk71nHVsh7KgI3GfA2zaIolsZ7PI"  # ใช้ตอนรันในเครื่อง
+    api_key = "GOOGLE_API_KEY"  # ใช้ตอนรันในเครื่อง
 
 if not api_key or api_key == "PUT_YOUR_GEMINI_API_KEY_HERE":
     st.error("❌ ยังไม่ได้ตั้งค่า GEMINI API KEY")
